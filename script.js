17ff1f59c74aad765fa952721547e62e7525ce2c
Zahra Malikzada <zara@Zahras-MacBook-Air.local>
2024-08-16 19:16:54 +0400

    second commit

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
-const button = document.getElementById("button");
-const pop_up = document.querySelector(".pop-up");
+// 1ci variant
 
-let timeOut = 1;
-let currentTime = new Date().getMinutes();
+// const button = document.getElementById("button");
+// const pop_up = document.querySelector(".pop-up");
 
-if(localStorage.getItem("isEntered") === null){
-    localStorage.setItem("isEntered", "false")
-}
-else{
-    if(localStorage.getItem("isEntered") === "false"){
-        button.onclick=()=>{
-            pop_up.remove();
-            localStorage.setItem("isEntered", "true");
-            localStorage.setItem("lastTime", new Date().getMinutes().toString())
-        }
-    }
-    else if(localStorage.getItem("isEntered")==="true"){
-        if(parseInt(localStorage.getItem("lastTime")) + timeOut <= currentTime){
-            localStorage.setItem("isEntered", "false");
-            pop_up.style.display = "block";
-        }
-        else {
-            pop_up.remove();
-        }
-    }
-}
+// let timeOut = 1;
+// let currentTime = new Date().getMinutes();
 
-
+// if (localStorage.getItem("isEntered") === null) {
+//   localStorage.setItem("isEntered", "false");
+// } 
+// else {
+//   if (localStorage.getItem("isEntered") === "false") {
+//     button.onclick = () => {
+//       pop_up.remove();
+//       localStorage.setItem("isEntered", "true");
+//       localStorage.setItem("lastTime", new Date().getMinutes().toString());
+//     };
+//   } 
+//   else if (localStorage.getItem("isEntered") === "true") {
+//     if (parseInt(localStorage.getItem("lastTime")) + timeOut <= currentTime) {
+//       localStorage.setItem("isEntered", "false");
+//       pop_up.style.display = "block";
+//     }
+//     else if(currentTime === 0){
+//         localStorage.setItem("isEntered", "false");
+//         pop_up.style.display = "block";
+//     }
+//     else {
+//       pop_up.remove();
+//     }
+//   }
+// }
 
 // localStorage.clear();
 
 
+// 2ci variant
+
+const button = document.getElementById("button");
+const pop_up = document.querySelector(".pop-up");
+
+
+
+if (localStorage.getItem("isEntered") === null) {
+  localStorage.setItem("isEntered", "false");
+} else {
+  if (localStorage.getItem("isEntered") === "false") {
+    button.onclick = () => {
+      pop_up.remove();
+      localStorage.setItem("isEntered", "true");
+    };      
+    setTimeout(() => {
+      pop_up.style.display = "block";
+      localStorage.setItem("isEntered", "false");
+    }, 60*1000);
+
+  } 
+  else if (localStorage.getItem("isEntered") === "true") {
+   pop_up.remove();
+  }
+}
+
+// localStorage.clear();
